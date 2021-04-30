cfbf0075a56ab34474e339ec42ffa211c560a6eb
abder <abderrahim.ouakki@datadoghq.com>
2021-04-30 23:37:48 +0200

    basic print and formatting

---
+++ print.rs
@@ -0,0 +1,17 @@
+pub fn show() {
+    // basic printing
+    println!("hello world");
+
+    // basic formatting
+    println!("this is a number {}", 1);
+
+    // named arguments
+    println!("my name is {} {}", "abderrahim", "ouakki");
+
+    // named arguments
+    println!("I like to play {sport}", sport = "football");
+
+    //Placehoder traints, specifying the type
+    println!("Binary {:b}, Hex: {:X}, Decimal:{:o}",100,100,100)
+
+}
